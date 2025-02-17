136048f8ed0d3a1673f7205d11219f404b2d16e4
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2025-02-16 23:18:28 -0300

    Create mario.c

---
+++ Módulo 1/mario.c
@@ -0,0 +1,24 @@
+// criando 4 blocos horizantais 
+
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
+}
+
+// criando 3 blocos verticais 
+
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        printf("#\n");
+    }
+}
